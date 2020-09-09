e5559bcec2a0ac85e1337b86b54dcd9dfbf8b009
shafo <shafo@posteo.net>
2020-09-09 21:09:14 +0200

    Emacs: add basic setup

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -13,26 +13,92 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;;; Core setup 
 ;; install use-package and use it by default for straight
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 
-;; store all backup and autosave files in the tmp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+;; Always follow symlinks. init files are normally stowed/symlinked.
+(setq vc-follow-symlinks t
+      find-file-visit-truename t
+      ;; Avoid stale compiled code shadow newer source code
+      load-prefer-newer t)
 
-;;; UI
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(setq scroll-step 1)
-(setq inhibit-startup-screen t)
-(setq scroll-preserve-screen-position 'always)
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;;; Basic Setup
+;;;; Emacs folder setup
+
+;; Define configuration directory. Emacs supports XDG_CONFIG_HOME since v27
+(setq my-emacs-conf-directory (expand-file-name "~/.config/emacs"))
+
+;;;; General settings
+(setq-default ;; Use setq-default to define global default
+ ;; Who I am
+ user-mail-address "shafo@posteo.net"
+ user-full-name "Shayan Fouladi"
+ ;; Enable all disabled commands
+ disabled-command-function nil
+ ;; Enable recursive minibuffer edit
+ enable-recursive-minibuffers t
+ ;; Don't show scratch message and remove echo area message
+ inhibit-startup-message t
+ inhibit-startup-echo-area-message t
+ initial-scratch-message 'nil
+ ;; Emacs modes typically provide a standard means to change the
+ ;; indentation width -- eg. c-basic-offset: use that to adjust your
+ ;; personal indentation width, while maintaining the style (and
+ ;; meaning) of any files you load.
+ indent-tabs-mode nil ; don't use tabs to indent
+ tab-width 8 ; but maintain correct appearance
+ ;; Use one space as sentence end
+ sentence-end-double-space 'nil
+ ;; Don't adjust window-vscroll to view tall lines.
+ auto-window-vscroll nil
+
+ ;; TODO Don't create lockfiles.
+ ;; recentf frequently prompts for confirmation.
+ ;;create-lockfiles nil
+ ;; Leave some rooms when recentering to top, useful in emacs ipython notebook.
+ ;; Move files to trash when deleting
+ delete-by-moving-to-trash t
+ ;; Show column number
+ column-number-mode t
+ ;; More message logs
+ message-log-max 16384
+ ;; Don't prompt up file dialog when click with mouse
+ use-file-dialog nil
+ ;; Place all auto-save files in /tmp directory and make them unique.
+ backup-directory-alist `((".*" . ,temporary-file-directory))
+ auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+ ;; more useful frame title, that show either a file or a
+ ;; buffer name (if the buffer isn't visiting a file)
+ frame-title-format '((:eval (if (buffer-file-name)
+                                 (abbreviate-file-name (buffer-file-name))
+                               "%b")))
+ ;; warn when opening files bigger than 100MB
+ large-file-warning-threshold 100000000
+ ;; Remember my location when the file is last opened
+ ;; activate it for all buffers
+ save-place t
+ ;; smooth scrolling
+ scroll-conservatively 101
+ ;; Reserve one line when scrolling
+ scroll-margin 1
+ ;; turn off the bell
+ ring-bell-function 'ignore
+ ;; Smoother scrolling
+ mouse-wheel-scroll-amount '(1 ((shift) . 1)) ;; one line at a time
+ mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
+ mouse-wheel-follow-mouse 't ;; scroll window under mouse
+ scroll-step 1 ;; keyboard scroll one line at a time
+ )
+
 (add-to-list 'default-frame-alist
                        '(font . "JetBrains Mono-10"))
 
+;;;; Extra packages
 (use-package outshine)
 (use-package magit)
